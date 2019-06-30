4c5eb0c1bd4bc4315d887d079770f98d9319dde7
dj0nny <frank96990@gmail.com>
2019-06-30 15:34:43 +0200

    :rocket: setup vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import records from './modules/records'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  modules: {
 
   },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  strict: true
 })
---
+++ src/store/modules/records.js
@@ -0,0 +1,9 @@
+export default {
+  state: {},
+
+  getters: {},
+
+  mutations: {},
+
+  actions: {}
+}
